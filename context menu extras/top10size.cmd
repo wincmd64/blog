e397406c8cdb3ad40b3d0b81d5db8e61a4af44a4
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-02-25 17:32:22 +0200

    Update top10size.cmd

--- context menu extras/top10size.cmd
+++ context menu extras/top10size.cmd
@@ -1,4 +1,4 @@
-:: Adds "Top 10 by Size (recursively)" entry to the Explorer context menu (hold Shift to activate)
+:: Adds "Top 10 by Size" entry to the Explorer context menu (hold Shift to activate)
 :: [area] dirs
 
 
@@ -6,11 +6,11 @@
 :: Use /u to undo changes
 if /i "%1"=="/u" goto undo
 
-reg add "HKCU\Software\Classes\Directory\shell\wincmd64_Top10size" /v "MUIVerb" /d "Top 10 by Size (recursively)" /f
+reg add "HKCU\Software\Classes\Directory\shell\wincmd64_Top10size" /v "MUIVerb" /d "Top 10 by Size" /f
 reg add "HKCU\Software\Classes\Directory\shell\wincmd64_Top10size" /v "Icon" /d "shell32.dll,133" /f
 reg add "HKCU\Software\Classes\Directory\shell\wincmd64_Top10size" /v "Position" /d "Bottom" /f
 reg add "HKCU\Software\Classes\Directory\shell\wincmd64_Top10size" /v "Extended" /f
-reg add "HKCU\Software\Classes\Directory\shell\wincmd64_Top10size\command" /ve /d "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command (Get-ChildItem -Path '%%V' -Recurse -File -ErrorAction SilentlyContinue | Select FullName,@{n='Size';e={$_.Length}}) + (Get-ChildItem -Path '%%V' -Directory -ErrorAction SilentlyContinue | ForEach {$_ | Add-Member Size ((Get-ChildItem $_.FullName -Recurse -File -ErrorAction SilentlyContinue | Measure-Object Length -Sum).Sum) -PassThru} | Select FullName,Size) | Sort Size -Desc | Select -First 10 @{n='Path';e={$_.FullName}},@{n='Size (MB)';e={'{0:N2}' -f ($_.Size / 1MB)}} | Format-Table -AutoSize | Out-Host; pause" /f
+reg add "HKCU\Software\Classes\Directory\shell\wincmd64_Top10size\command" /ve /d "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command (Get-ChildItem -Path '%%V' -File -ErrorAction SilentlyContinue | Select FullName,@{n='Size';e={$_.Length}}) + (Get-ChildItem -Path '%%V' -Directory -ErrorAction SilentlyContinue | ForEach {$_ | Add-Member Size ((Get-ChildItem $_.FullName -Recurse -File -ErrorAction SilentlyContinue | Measure-Object Length -Sum).Sum) -PassThru} | Select FullName,Size) | Sort Size -Desc | Select -First 10 @{n='Path';e={$_.FullName}},@{n='Size (MB)';e={'{0:N2}' -f ($_.Size / 1MB)}} | Format-Table -AutoSize | Out-Host; pause" /f
 
 color 27 & timeout 1 & exit
 
