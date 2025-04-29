c5061472df5a5eb71ddec11407ccea7b4b8c92d3
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-04-29 10:24:39 +0300

    Add files via upload

---
+++ context_menu_extras/del_empty_folders.cmd
@@ -0,0 +1,19 @@
+:: Adds "Delete empty folders (recursively)" entry to the Explorer context menu (hold Shift to activate)
+:: [area] dirs
+
+
+@ECHO OFF
+:: Use /u to undo changes
+if /i "%1"=="/u" goto undo
+
+reg add "HKCU\Software\Classes\Directory\shell\wincmd64_DelEmptyDirs" /v "MUIVerb" /d "Delete empty folders (recursively)" /f
+reg add "HKCU\Software\Classes\Directory\shell\wincmd64_DelEmptyDirs" /v "Icon" /d "imageres.dll,7" /f
+reg add "HKCU\Software\Classes\Directory\shell\wincmd64_DelEmptyDirs" /v "Position" /d "Bottom" /f
+reg add "HKCU\Software\Classes\Directory\shell\wincmd64_DelEmptyDirs" /v "Extended" /f
+reg add "HKCU\Software\Classes\Directory\shell\wincmd64_DelEmptyDirs\command" /ve /d "cmd.exe /K for /f \"usebackq delims=\" %%%%d in (`\"dir \"%%V\" /ad/b/s ^| sort /R\"`) do rd \"%%%%~d\"" /f
+
+color 27 & timeout 1 & exit
+
+:undo
+reg delete "HKCU\Software\Classes\Directory\shell\wincmd64_DelEmptyDirs" /f
+color 27 & timeout 1
