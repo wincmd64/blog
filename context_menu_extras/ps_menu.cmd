6ada711a7ff9cc98b23a43b01379490e8d16724b
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-05-01 14:47:11 +0300

    Add files via upload

--- context_menu_extras/ps_menu.cmd
+++ context_menu_extras/ps_menu.cmd
@@ -14,15 +14,15 @@ reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun"
 
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\01u" /v "MUIVerb" /d "NoExit" /f
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\01u" /v "Icon" /d "powershell.exe" /f
-reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\01u\command" /ve /d "powershell.exe -NoExit -ExecutionPolicy Bypass -File \"%%1\"" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\01u\command" /ve /d "powershell.exe -NoE -Ep Bypass -File \"%%1\"" /f
 
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\02a" /v "MUIVerb" /d "Admin" /f
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\02a" /v "Icon" /d "powershell.exe,1" /f
-reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\02a\command" /ve /d "powershell.exe -Command \"Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \\\"%%1\\\"' -Verb RunAs\"" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\02a\command" /ve /d "powershell.exe -Command \"Start-Process powershell.exe -ArgumentList '-Ep Bypass -File \\\"%%1\\\"' -Verb RunAs\"" /f
 
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\03a" /v "MUIVerb" /d "Admin NoExit" /f
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\03a" /v "Icon" /d "powershell.exe,1" /f
-reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\03a\command" /ve /d "powershell.exe -Command \"Start-Process powershell.exe -ArgumentList '-NoExit -ExecutionPolicy Bypass -File \\\"%%1\\\"' -Verb RunAs\"" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSrun\shell\03a\command" /ve /d "powershell.exe -Command \"Start-Process powershell.exe -ArgumentList '-NoExit -Ep Bypass -File \\\"%%1\\\"' -Verb RunAs\"" /f
 
 :: ExecutionPolicy
 
@@ -33,15 +33,15 @@ reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexec
 
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\01get" /v "MUIVerb" /d "Get Current" /f
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\01get" /v "Icon" /d "powershell.exe" /f
-reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\01get\command" /ve /d "powershell.exe -NoExit -Command \"write-host 'Current execution policy: ' -NoNewline; Get-ExecutionPolicy ^| write-host -ForegroundColor Yellow; ^''\"" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\01get\command" /ve /d "powershell.exe -NoE -NoP -C \"Get-ExecutionPolicy -List\"" /f
 
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\02set" /v "MUIVerb" /d "Set RemoteSigned" /f
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\02set" /v "Icon" /d "powershell.exe" /f
-reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\02set\command" /ve /d "powershell.exe -NoExit -Command \"Set-ExecutionPolicy RemoteSigned -Scope CurrentUser; write-host 'Set as default: ' -NoNewline; Get-ExecutionPolicy ^| write-host -ForegroundColor Green; ^''\"" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\02set\command" /ve /d "powershell.exe -NoP -C \"Set-ExecutionPolicy RemoteSigned -Scope CurrentUser; Write-Host `n' Set as default: ' -NoNewline; Get-ExecutionPolicy ^| Write-host -ForegroundColor Green; Write-Host; pause\"" /f
 
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\03set" /v "MUIVerb" /d "Set Restricted" /f
 reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\03set" /v "Icon" /d "powershell.exe" /f
-reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\03set\command" /ve /d "powershell.exe -NoExit -Command \"Set-ExecutionPolicy Restricted -Scope CurrentUser; write-host 'Set as default: ' -NoNewline; Get-ExecutionPolicy ^| write-host -ForegroundColor Red; ^''\"" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.ps1\shell\wincmd64_PSexecpol\shell\03set\command" /ve /d "powershell.exe -NoP -C \"Set-ExecutionPolicy Restricted -Scope CurrentUser; Write-Host `n' Set as default: ' -NoNewline; Get-ExecutionPolicy ^| Write-host -ForegroundColor Red; Write-Host; pause\"" /f
 
 color 27 & timeout 1 & exit
 
