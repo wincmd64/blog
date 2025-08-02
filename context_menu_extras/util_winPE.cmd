31ec4bda123f74d8f8a53883d65868f6c15717b1
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-08-02 10:38:10 +0300

    Update util_winPE.cmd

--- context_menu_extras/util_winPE.cmd
+++ context_menu_extras/util_winPE.cmd
@@ -6,13 +6,13 @@
 :: Use /u to undo changes
 if /i "%1"=="/u" goto undo
 
-for /f "tokens=* delims=" %%a in ('where TBWinPE.exe 2^>nul') do set "TBWinPE=%%a"
-if exist "%TBWinPE%" (
-    reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM" /v "MUIVerb" /d "Boot now!" /f
-    reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM" /v "Icon" /d "%TBWinPE%" /f
-    reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM" /v "Position" /d "Bottom" /f
-    reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM\command" /ve /d "\"%TBWinPE%\" /bootwim \"%%1\"" /f
-) else (color 4 & echo. & echo  TBWinPE.exe not found. Try download from: https://www.terabyteunlimited.com/downloads/wp/tbwinpe.zip & echo. & pause & exit)
+for /f "tokens=* delims=" %%a in ('where TBWinPE.exe 2^>nul') do set "app=%%a"
+if not exist "%app%" (color 4 & echo. & echo  TBWinPE.exe not found. Try download from: https://www.terabyteunlimited.com/downloads/wp/tbwinpe.zip & echo. & pause & exit)
+
+reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM" /v "MUIVerb" /d "Boot now!" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM" /v "Icon" /d "%app%" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM" /v "Position" /d "Bottom" /f
+reg add "HKCU\Software\Classes\SystemFileAssociations\.wim\shell\wincmd64_WIM\command" /ve /d "\"%app%\" /bootwim \"%%1\"" /f
 
 color 27 & timeout 1 & exit
 
